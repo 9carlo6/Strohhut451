00c191b77ed48e4afc4a2cc6c440f054e07bcf53
9carlo6 <carlopannullo809@gmail.com>
2021-10-21 19:20:19 +0200

    Risoluzione problema danno multiplo del nemico
    Aggiunta di un timer per la gestione del danno inflitto al personaggio da parte del nemico (bisogna controllare se la logica dello stato "EnemyMeleeAttackState" è concorde con gli altri)

--- MontagsGame/Assets/Script/EnemyScripts/EnemyStateMachine/EnemyMeleeAttackState.cs
+++ MontagsGame/Assets/Script/EnemyScripts/EnemyStateMachine/EnemyMeleeAttackState.cs
@@ -16,6 +16,9 @@ public class EnemyMeleeAttackState : EnemyBaseState
   	public Animator animator;
     private AnimatorClipInfo[] clipInfo;
 
+    //Questo è il timer per gestire quando infliggere il danno al player
+    public float timeRemainingToAttack;
+
     public override void EnterState(EnemyStateManager enemy)
     {
         Debug.Log("Stato Nemico = Attacca");
@@ -25,6 +28,10 @@ public class EnemyMeleeAttackState : EnemyBaseState
         enemyHealthManager = enemy.GetComponent<EnemyHealthManager>();
         enemyController = enemy.GetComponent<EnemyController>();
         animator = enemy.GetComponent<Animator>();
+
+        //Il timer viene settato a un valore iniziale (che dovrebbe coincidire con la lunghezza dell'animazione "AttaccoDirettoNemico")
+        //Bisogna trovare un modo per ricavare la lunghezza di una specifica animazione (ora sappiamo ricavare solo quella dell'animazione corrente)
+        timeRemainingToAttack = 0.5f;
     }
 
     public override void UpdateState(EnemyStateManager enemy)
@@ -33,30 +40,43 @@ public class EnemyMeleeAttackState : EnemyBaseState
         distanceToTarget = Vector3.Distance(enemyTransform.position, targetTransform.position);
 
         if (distanceToTarget <= 1.5){
-          enemyTransform.LookAt(targetTransform);
-
-          if(string.Equals(GetCurrentClipName(), "AttaccoDirettoNemico") && animator.GetCurrentAnimatorStateInfo(0).normalizedTime >= 0.99f){
-            Collider[] hitPlayer = Physics.OverlapSphere(enemyController.attackPoint.position, enemyController.attackRange, enemyController.targetMask);
-
-            if(hitPlayer.Length != 0)
-            {
-                Debug.Log("Sto colpendo il player con melee");
-                playerRef.GetComponent<PlayerHealthManager>().HurtPlayer(enemyController.meleeDamage);
-            }
-          }
-        }else{
+            enemyTransform.LookAt(targetTransform);
+            EnemyAttack();
+        }
+        else{
             enemy.SwitchState(enemy.ChasePlayerState);
         }
 
         if(enemyHealthManager.currentHealth <= 0){
             enemy.SwitchState(enemy.DeathState);
         }
+    }
 
+    //Funzione per gestire il danno inflitto al personaggio dall'attacco del nemico
+    public void EnemyAttack()
+    {
+        //Se il tempo per il prossimo attacco è ancora maggiore di zero allo si diminuisce timer
+        if (timeRemainingToAttack > 0)
+        {
+            timeRemainingToAttack -= Time.deltaTime;
+        }
+        //Altrimenti si procede infliggendo il danno al nemico
+        else
+        {
+            if (string.Equals(GetCurrentClipName(), "AttaccoDirettoNemico"))
+            {
+                Collider[] hitPlayer = Physics.OverlapSphere(enemyController.attackPoint.position, enemyController.attackRange, enemyController.targetMask);
 
-        //ATTACCO
+                if (hitPlayer.Length != 0)
+                {
+                    Debug.Log("Sto colpendo il player con melee");
+                    playerRef.GetComponent<PlayerHealthManager>().HurtPlayer(enemyController.meleeDamage);
 
-
-         //FINE ATTACCO
+                    //Una volta inflitto il danno il timer aggiornato alla lunghezza dell'animazione corrente, ovvero "AttaccoDirettoNemico"
+                    timeRemainingToAttack = animator.GetCurrentAnimatorStateInfo(0).length;
+                }
+            }
+        }
     }
 
 
