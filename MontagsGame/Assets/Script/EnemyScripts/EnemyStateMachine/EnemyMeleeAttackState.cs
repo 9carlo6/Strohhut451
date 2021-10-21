95e24614d914d2a8e824592a2d0254f595d2ac71
9carlo6 <carlopannullo809@gmail.com>
2021-10-21 18:19:11 +0200

    Inizio risoluzione problema danno multiplo del nemico

--- MontagsGame/Assets/Script/EnemyScripts/EnemyStateMachine/EnemyMeleeAttackState.cs
+++ MontagsGame/Assets/Script/EnemyScripts/EnemyStateMachine/EnemyMeleeAttackState.cs
@@ -4,16 +4,17 @@ using UnityEngine;
 
 public class EnemyMeleeAttackState : EnemyBaseState
 {
-
     float distanceToTarget;
     Transform enemyTransform;
     Transform targetTransform;
     PlayerController playerController;
     GameObject playerRef;
     EnemyHealthManager enemyHealthManager;
+    EnemyController enemyController;
 
-
-
+    //per l'animazione
+  	public Animator animator;
+    private AnimatorClipInfo[] clipInfo;
 
     public override void EnterState(EnemyStateManager enemy)
     {
@@ -22,8 +23,8 @@ public class EnemyMeleeAttackState : EnemyBaseState
         playerRef = GameObject.FindGameObjectWithTag("Player");
         targetTransform = playerRef.transform;
         enemyHealthManager = enemy.GetComponent<EnemyHealthManager>();
-
-
+        enemyController = enemy.GetComponent<EnemyController>();
+        animator = enemy.GetComponent<Animator>();
     }
 
     public override void UpdateState(EnemyStateManager enemy)
@@ -31,31 +32,39 @@ public class EnemyMeleeAttackState : EnemyBaseState
 
         distanceToTarget = Vector3.Distance(enemyTransform.position, targetTransform.position);
 
+        if (distanceToTarget <= 1.5){
+          enemyTransform.LookAt(targetTransform);
 
-        if (distanceToTarget <= 1.5)
-        {
-            Attach();
-        }
-        else
-        {
+          if(string.Equals(GetCurrentClipName(), "AttaccoDirettoNemico") && animator.GetCurrentAnimatorStateInfo(0).normalizedTime >= 0.99f){
+            Collider[] hitPlayer = Physics.OverlapSphere(enemyController.attackPoint.position, enemyController.attackRange, enemyController.targetMask);
+
+            if(hitPlayer.Length != 0)
+            {
+                Debug.Log("Sto colpendo il player con melee");
+                playerRef.GetComponent<PlayerHealthManager>().HurtPlayer(enemyController.meleeDamage);
+            }
+          }
+        }else{
             enemy.SwitchState(enemy.ChasePlayerState);
         }
-        //}
 
-        if(enemyHealthManager.currentHealth <= 0)
-        {
+        if(enemyHealthManager.currentHealth <= 0){
             enemy.SwitchState(enemy.DeathState);
         }
 
+
+        //ATTACCO
+
+
+         //FINE ATTACCO
     }
 
-    public void Attach()
-    {
-
-        Debug.Log("ZAC ZAC");
-        enemyTransform.LookAt(targetTransform);
 
 
+    //Funzione necessaria per risalire al nome dell'animazione corrente
+    public string GetCurrentClipName(){
+      clipInfo = animator.GetCurrentAnimatorClipInfo(0);
+      return clipInfo[0].clip.name;
     }
 
     public override void OnCollisionEnter(EnemyStateManager enemy, Collision collision)
