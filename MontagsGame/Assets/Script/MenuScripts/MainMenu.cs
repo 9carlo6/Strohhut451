872e5634eeaae4a3d13a9d9674d304035f169848
9carlo6 <carlopannullo809@gmail.com>
2021-10-22 17:32:56 +0200

    Modifiche Menu

--- MontagsGame/Assets/Script/MenuScripts/MainMenu.cs
+++ MontagsGame/Assets/Script/MenuScripts/MainMenu.cs
@@ -5,6 +5,17 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    public float moveSpeed = 10f;
+    public float turnSpeed = 2f;
+
+    [HideInInspector] public float timeCounterMovement;
+    [HideInInspector] public float timeCounterRotation;
+    [HideInInspector] public float defaultTimeCounterMovement = 2;
+    [HideInInspector] public float defaultTimeCounterRotation = 1;
+    [HideInInspector] public bool leftDirection;
+    [HideInInspector] public bool forwardDirection;
+
+
     public void StartGame()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
@@ -15,4 +26,65 @@ public class MainMenu : MonoBehaviour
         Debug.Log("Uscita dal Gioco");
         Application.Quit();
     }
+
+    void Start()
+    {
+        timeCounterMovement = defaultTimeCounterMovement;
+        timeCounterRotation = defaultTimeCounterRotation;
+        leftDirection = true;
+    }
+
+    void Update()
+    {
+        handleLateralMovement();
+        handleOndulatoryMovement();
+    }
+
+    void handleLateralMovement()
+    {
+        if (timeCounterMovement > 0)
+        {
+            timeCounterMovement -= Time.deltaTime;
+
+            if (leftDirection)
+            {
+                transform.Translate(Vector3.left * moveSpeed * Time.deltaTime);
+            }
+            else
+            {
+                transform.Translate(Vector3.right * moveSpeed * Time.deltaTime);
+            }
+        }
+        else
+        {
+            leftDirection = !leftDirection;
+
+            //Deve essere il doppio in quanto partendo dal centro impieghera un tempo paria defaultTimeCounter per arrivare a sinistra
+            //e successivamente un tempo pari a defaultTimeCounter * 2 per arrivare a destra (poi sarà sempre cosi)
+            timeCounterMovement = defaultTimeCounterMovement * 2;
+        }
+    }
+
+    void handleOndulatoryMovement()
+    {
+        if (timeCounterRotation > 0)
+        {
+            timeCounterRotation -= Time.deltaTime;
+
+            if (forwardDirection)
+            {
+                transform.Rotate(Vector3.forward * turnSpeed * Time.deltaTime);
+            }
+            else
+            {
+                transform.Rotate(Vector3.back * turnSpeed * Time.deltaTime);
+            }
+        }
+        else
+        {
+            forwardDirection = !forwardDirection;
+            timeCounterRotation = defaultTimeCounterRotation * 2;
+        }
+    }
+
 }
