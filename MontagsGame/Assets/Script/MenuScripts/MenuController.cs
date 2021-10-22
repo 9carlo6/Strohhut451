fdd7070153ad201e71827968ec8f2a2d7f3026dd
9carlo6 <carlopannullo809@gmail.com>
2021-10-22 20:58:40 +0200

    Aggiunta titolo gioco

---
+++ MontagsGame/Assets/Script/MenuScripts/MenuController.cs
@@ -0,0 +1,90 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuController : MonoBehaviour
+{
+    public float moveSpeed = 10f;
+    public float turnSpeed = 2f;
+
+    [HideInInspector] public float timeCounterMovement;
+    [HideInInspector] public float timeCounterRotation;
+    [HideInInspector] public float defaultTimeCounterMovement = 2;
+    [HideInInspector] public float defaultTimeCounterRotation = 1;
+    [HideInInspector] public bool leftDirection;
+    [HideInInspector] public bool forwardDirection;
+
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void ExitGame()
+    {
+        Debug.Log("Uscita dal Gioco");
+        Application.Quit();
+    }
+
+    void Start()
+    {
+        timeCounterMovement = defaultTimeCounterMovement;
+        timeCounterRotation = defaultTimeCounterRotation;
+        leftDirection = true;
+    }
+
+    void Update()
+    {
+        handleLateralMovement();
+        handleOndulatoryMovement();
+    }
+
+    void handleLateralMovement()
+    {
+        if (timeCounterMovement > 0)
+        {
+            timeCounterMovement -= Time.deltaTime;
+
+            if (leftDirection)
+            {
+                transform.Translate(Vector3.left * moveSpeed * Time.deltaTime);
+            }
+            else
+            {
+                transform.Translate(Vector3.right * moveSpeed * Time.deltaTime);
+            }
+        }
+        else
+        {
+            leftDirection = !leftDirection;
+
+            //Deve essere il doppio in quanto partendo dal centro impieghera un tempo paria defaultTimeCounter per arrivare a sinistra
+            //e successivamente un tempo pari a defaultTimeCounter * 2 per arrivare a destra (poi sarà sempre cosi)
+            timeCounterMovement = defaultTimeCounterMovement * 2;
+        }
+    }
+
+    void handleOndulatoryMovement()
+    {
+        if (timeCounterRotation > 0)
+        {
+            timeCounterRotation -= Time.deltaTime;
+
+            if (forwardDirection)
+            {
+                transform.Rotate(Vector3.forward * turnSpeed * Time.deltaTime);
+            }
+            else
+            {
+                transform.Rotate(Vector3.back * turnSpeed * Time.deltaTime);
+            }
+        }
+        else
+        {
+            forwardDirection = !forwardDirection;
+            timeCounterRotation = defaultTimeCounterRotation * 2;
+        }
+    }
+
+}
