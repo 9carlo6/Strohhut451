a56df52cf60ab207706f0e963fe9b32d7254d200
9carlo6 <carlopannullo809@gmail.com>
2021-11-01 18:47:42 +0100

    Aggiunta combo e controllo numero nemici rimanetni UI

--- MontagsGame/Assets/Script/LevelScript/LevelController.cs
+++ MontagsGame/Assets/Script/LevelScript/LevelController.cs
@@ -8,9 +8,20 @@ public class LevelController : MonoBehaviour
     private GameObject levelInfoCanvas;
 
     //Per gestire i punti accumulati nel livello
-    public GameObject pointText;
+    public GameObject pointsText;
     public int levelPoints = 0;
-    public float timeRemaining = 10;
+
+    //Per gestire la combo
+    public GameObject comboText;
+    public float comboTimeCounter = 0;
+    public int comboMultiplier = 0;
+
+    //Per gestire i vari tipi di punteggio
+    public int enemyKillScore = 250;
+
+    //Per controllare il numero corrente di nemici presenti nel livello
+    private int currentNumberOfEnemies;
+    public GameObject enemiesNumberText;
 
 
     // Start is called before the first frame update
@@ -19,17 +30,52 @@ public class LevelController : MonoBehaviour
         //Per accedere al canvas relativo alle info del livello
         levelInfoCanvas = transform.Find("LevelInfoCanvas").gameObject;
 
+        //Per prendere il numero corrente di nemici presenti nel livello
+        currentNumberOfEnemies = GameObject.FindGameObjectsWithTag("Enemy").Length;
+
+        //Aggiorna il numero di nemici sulla UI
+        enemiesNumberText.GetComponent<TMP_Text>().text = new string('*', currentNumberOfEnemies);
     }
 
     // Update is called once per frame
     void Update()
     {
-        //Per modificare il valore del testo relativo ai punti accumulati nel livello (DA RIMUOVERE CONTATORE)
-        if (timeRemaining > 0)
+        //Per gestire i punti da assegnare ogni volta che si uccide un nemico
+       if (currentNumberOfEnemies != GameObject.FindGameObjectsWithTag("Enemy").Length)
         {
-            timeRemaining -= Time.deltaTime;
+            //Per prendere il numero corrente di nemici presenti nel livello
+            currentNumberOfEnemies = GameObject.FindGameObjectsWithTag("Enemy").Length;
+
+            //Il moltiplicatore viene incrementato a ogni nemico ucciso
+            comboMultiplier++;
+
+            //Per aggiornare il valore del moltiplicatore sulla UI
+            comboText.GetComponent<TMP_Text>().text = (comboMultiplier + 1).ToString() + " " + "X";
+
+            //Il punti vengono aggiornato 
+            levelPoints += enemyKillScore * comboMultiplier;
+
+            //Quando si uccide un nemico il contatore viene riportato a 5 secondi
+            comboTimeCounter = 5;
+
+            //Aggiorna il numero di nemici sulla UI
+            enemiesNumberText.GetComponent<TMP_Text>().text = new string('*', currentNumberOfEnemies);
         }
 
-        pointText.GetComponent<TMP_Text>().text = timeRemaining.ToString("0") + " " + "PT";
+        //Per modificare il valore del testo relativo ai punti accumulati nel livello (DA RIMUOVERE CONTATORE)
+        if (comboTimeCounter > 0)
+        {
+            comboTimeCounter -= Time.deltaTime;
+        }
+        else
+        {
+            //Se il valore del contatore arriva a 0 il moltiplicatore combo viene messo a 0
+            comboMultiplier = 0;
+
+            //Per aggiornare il valore del moltiplicatore sulla UI
+            comboText.GetComponent<TMP_Text>().text = " ";
+        }
+
+        pointsText.GetComponent<TMP_Text>().text = levelPoints.ToString() + " " + "PT";
     }
 }
