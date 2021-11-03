8faa2bfe7473754b2043630e6b9d3d5253e0da45
9carlo6 <carlopannullo809@gmail.com>
2021-11-03 09:37:15 +0100

    Aggiunta feature al player

---
+++ MontagsGame/Assets/Push-To-Data/Feature/Human/HumanFeaturesJsonMap.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+[Serializable]
+public class HumanFeaturesJsonMap
+{
+  public float FT_SPEED;
+  public float FT_HEALTH;
+  public float FT_ATTACK_RANGE;
+  public float FT_MELEE_DAMAGE;
+
+}
--- MontagsGame/Assets/Script/PlayerScripts/PlayerHealthManager.cs
+++ MontagsGame/Assets/Script/PlayerScripts/PlayerHealthManager.cs
@@ -1,22 +1,39 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.IO;
 
 public class PlayerHealthManager : MonoBehaviour
 {
     public float startingHealth;
     public float currentHealth;
 
+    //Per gestire le feature;
+  	public Dictionary<string, HumanFeature> features;
+  	private HumanFeaturesJsonMap humanMapper;
+
+
     // Start is called before the first frame update
     void Start()
     {
+        //Inizio - Inizializzazione delle feature
+    		string fileString = new StreamReader("Assets/Push-To-Data/Feature/Human/player_features.json").ReadToEnd();
+    		humanMapper = JsonUtility.FromJson<HumanFeaturesJsonMap>(fileString);
+
+    		features = new Dictionary<string, HumanFeature>();
+    		features.Add("startingHealth", new HumanFeature(humanMapper.FT_HEALTH, HumanFeature.FeatureType.FT_HEALTH));
+
+    		//Da eliminare???
+    		startingHealth = (float) features["startingHealth"].currentValue;
+    		//Fine - Inizializzazione delle feature
+
         currentHealth = startingHealth;
     }
 
     // Update is called once per frame
     void Update()
     {
-        //abbassare barra vita grafica se ci sarà
+        //abbassare barra vita grafica se ci sarï¿½
     }
 
     public void HurtPlayer(float damageAmount)
